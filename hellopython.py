c68da32e02d9c31d7547062eb34874459d597ede
JULYENJUYNN <41898754+JULYENJUYNN@users.noreply.github.com>
2018-07-30 18:29:58 +0800

    Update hellopython.py

--- hellopython.py
+++ hellopython.py
@@ -1,10 +1,11 @@
 def fact(n):
-  if n<=0:
-    print('ValeError')
-  if n==1:
-    return 1
-  else:
-    return n*fact(n-1)
+    if n<=0:
+        print('ValeError')
+    if n==1:
+        return 1
+    else:
+        return n*fact(n-1)
 n = 5
 mult=fact(n)
 print('% 的阶乘是%',n,mult)
+    
